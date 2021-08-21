00600683cde7a84c00a0e91b37c5d6fa6ab50ea8
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-08-22 01:16:44 +0300

    finishing

--- app.js
+++ app.js
@@ -1,114 +1,145 @@
 'use strict';
-
 let attemptEl = document.getElementById('attempts');
-let container = document.getElementById('');
+let container = document.getElementById('image-container');
 let fImg = document.getElementById('fImg');
 let sImg = document.getElementById('sImg');
 let thImg = document.getElementById('thImg');
 let result = document.getElementById('results');
+ let image = ['bag.jpg', 'banana.jpg', 'breakfast.jpg', 'bathroom.jpg', 'boots.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg ', 'unicorn.jpg', 'water-can.jpg ', 'wine-glass.jpg'];
 
-let arrImg = ['bag.jpg', 'banana.jpg', 'breakfast.jpg', 'bathroom.jpg', 'boots.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg ', 'unicorn.jpg', 'water-can.jpg ', 'wine-glass.jpg'];
-
-let attempt = 1;
-let maxAttempt = 25;
-let saveImages = [];
-
-function Image(imgName) {
-
-    this.imgName = imgName.split('.')[0];
-    this.img = `images/${imgName}`;
+let max = 25;
+let attempt = 1
+let bus = [];
+let aNames = [];
+let vote = [];
+let view = [];
+let ulEl = document.getElementById('bus');
+function saveToLocalStorage() {
+  
+    let data = JSON.stringify(bus);
+    localStorage.setItem('busMall', data);
+}
+function readFromLocalStorage() {
+    let stringObject = localStorage.getItem('busMall');
+    let normalObject = JSON.parse(stringObject);
+    if (normalObject) {
+        bus = normalObject;
+        renderImg();
+    }
+}
+function Image(name) {
+    this.bName = name.split('.')[0];
+    this.bImg = `images/${name}`;
     this.votes = 0;
     this.views = 0;
-    saveImages.push(this);
-
+    bus.push(this);
+    aNames.push(this.bName);
 }
-
-for (let i = 0; i < arrImg.length; i++) {
-
-    new Image(arrImg[i]);
+for (let i = 0; i < image.length; i++) {
+    new Image(image[i]);
+    console.log(image[i]);
 }
-
-function gitRandomImage() {
-
-    return Math.floor(Math.random() * arrImg.length);
+function randomImage() {
+    return Math.floor(Math.random() * bus.length);
 }
-
-let firstIndex;
-let secondIndex;
-let thirdIndex;
-
-function renderImage() {
-
-    firstIndex = gitRandomImage();
-    secondIndex = gitRandomImage();
-    thirdIndex = gitRandomImage();
-
-    while (firstIndex === secondIndex || secondIndex === thirdIndex || thirdIndex === firstIndex) {
-
-        firstIndex = gitRandomImage();
-        thirdIndex = gitRandomImage();
+let leftIndex;
+let rightIndex;
+let middleIndex;
+let ran = [];
+function renderImg() {
+    while (leftIndex === middleIndex ||
+        middleIndex === rightIndex ||
+        leftIndex === rightIndex || ran.includes(leftIndex) ||
+        ran.includes(middleIndex) ||
+        ran.includes(rightIndex)) {
+        leftIndex = randomImage();
+        rightIndex = randomImage();
+        middleIndex = randomImage();
     }
-
-    fImg.setAttribute('src', saveImages[firstIndex].img)
-    sImg.setAttribute('src', saveImages[secondIndex].img);
-    thImg.setAttribute('src', saveImages[thirdIndex].img);
-
-    saveImages[firstIndex].views++;
-    saveImages[secondIndex].views++;
-    saveImages[thirdIndex].views++;
- 
+    ran[0] = (leftIndex);
+    ran[1] = (rightIndex);
+    ran[2] = (middleIndex);
+    leftImg.setAttribute('src', bus[leftIndex].bImg);
+    rightImg.setAttribute('src', bus[rightIndex].bImg);
+    middleImg.setAttribute('src', bus[middleIndex].bImg);
+    bus[leftIndex].views++;
+    bus[rightIndex].views++;
+    bus[middleIndex].views++;
 }
-renderImage();
 
-fImg.addEventListener('click', clickHandler);
-sImg.addEventListener('click', clickHandler);
-thImg.addEventListener('click', clickHandler);
-
+renderImg();
+leftImg.addEventListener('click', clickHandler);
+rightImg.addEventListener('click', clickHandler);
+middleImg.addEventListener('click', clickHandler);
 function clickHandler(event) {
-
-    if (attempt <= maxAttempt) {
-
-        let clik = event.target.id;
-
-        if (clik === 'fImg') {
-
-            saveImages[firstIndex].votes++;
-
+    if (attempt <= max) {
+        btn.addEventListener('click',showingResult);
+        let clickedImage = event.target.id;
+        if (clickedImage === 'leftImg') {
+            bus[leftIndex].votes++;
+        } else if (clickedImage === 'rightImg') {
+            bus[rightIndex].votes++;
+        } else if (clickedImage === 'middleImg') {
+            bus[middleIndex].votes++;
         }
-
-        else if (clik === 'sImg') {
-
-            saveImages[secondIndex].votes++
-
-        }
-
-        else if (clik === 'thImg') {
-
-            saveImages[thirdIndex].votes++
-
-        }
-
-        renderImage();
-
+        renderImg();
         attempt++;
 
+    } else {
+        leftImg.removeEventListener('click', clickHandler);
+        rightImg.removeEventListener('click', clickHandler);
+        middleImg.removeEventListener('click', clickHandler);
     }
 }
+let btn = document.getElementById('btn');
 
-let resultButton = document.getElementById('button');
-
-resultButton.addEventListener('click', showResult);
-
-function showResult() {
-
-    for (let i = 0; i < saveImages.length; i++) {
-
+function showingResult() {
+    for (let i = 0; i < bus.length; i++) {
         let liEl = document.createElement('li');
-
         result.appendChild(liEl);
-
-        liEl.textContent = `${saveImages[i].imgName} has ${saveImages[i].votes} votes and  ${saveImages[i].views} views.`;
-
+        liEl.textContent = `${bus[i].bName} has ${bus[i].votes} votes and ${bus[i].views} views.`;
+        vote.push(bus[i].votes);
+        view.push(bus[i].views);
     }
-
+    btn.removeEventListener('click', showingResult);
+    saveToLocalStorage();
+    chartRender();
 }
+function chartRender() {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: aNames,
+            datasets: [{
+                label: '# of Votes',
+                data: vote,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of views',
+                data: view,
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+readFromLocalStorage();
